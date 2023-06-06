64df28a5ccd087f982ddc508bc35b5da33489103
Ibrahim Muradov <131617382+ibrahimmuradov@users.noreply.github.com>
2023-06-06 13:20:13 +0400

    Update settings.py secret key

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '' # secret key
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', get_random_secret_key()) # secret key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
